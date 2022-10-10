3629290beac50274cb4293c74012a3a5f22f759e
DLLM <DLLM@gft.com>
2022-10-10 16:25:08 -0300

    Utilizando os hooks useState e UseEfect para manipular o carregamento de dados e realizando uma refatoração receber o topo no componente de produtores, e flatlist

--- src/telas/Home/index.js
+++ src/telas/Home/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import Produtores from './components/Produtores.js';
 import Topo from './components/Topo.js';
 
 export default function Home() {
-  return <Topo />;
+  return (
+    <>
+      <Produtores topo={Topo} />
+    </>
+  );
 }
