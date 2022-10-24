882394b6752512e4e60e6786f7466d1d7652314d
DLLM <DLLM@gft.com>
2022-10-24 11:08:08 -0300

    Adicionando os mocks aos testes mock implementation e requisicoes

--- __tests__/repositorio/leilao.test.js
+++ __tests__/repositorio/leilao.test.js
@@ -1,4 +1,5 @@
 import { obtemLeilao, obtemLeiloes } from "../../src/repositorio/leilao";
+import apiLeiloes from '../../src/servicos/apiLeiloes';
 
 // jest.mock('../../src/servicos/apiLeiloes.js', () => {
 
@@ -14,12 +15,43 @@ const mockLeiloes = [
     }
 ]
 
+const mockRequisicao = (retorno) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({
+                data: retorno
+            })
+        }, 200);
+    });
+}
+
+const mockRequisicaoError = () => {
+    return new Promise((_, reject) => {
+        setTimeout(() => {
+            reject();
+        }, 200);
+    });
+}
+
+
 
 describe("repositorio/leilao", () => {
     describe("Obtem leiloes", () => {
         it('Deve retornor uma lista de leilões', async () => {
+            apiLeiloes.get.mockImplementation(() => mockRequisicao(mockLeiloes))
+
             const leiloes = await obtemLeiloes();
-            console.log(leiloes);
+
+            expect(leiloes).toEqual(mockLeiloes);
         });
+
+        it('Deve retornor uma lista vazia quando a requisição falhar', async () => {
+            apiLeiloes.get.mockImplementation(() => mockRequisicaoError())
+
+            const leiloes = await obtemLeiloes();
+
+            expect(leiloes).toEqual([]);
+        });
+
     });
 });
